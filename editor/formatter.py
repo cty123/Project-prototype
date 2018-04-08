d7eb4b63a5c582da98c62e2a18eb5ed5ff34bacc
achesak <achesak@yahoo.com>
2018-04-08 13:58:00 -0400

    Combine error + output for compile and execution

--- editor/formatter.py
+++ editor/formatter.py
@@ -3,9 +3,9 @@ def format(output):
 
     result = ""
     for section in output:
-        if section[1]:
+        if section[1] and section[1].strip() != "":
             result += "<p><h1 class=\"c-heading u-small\">" + section[0] + "</h1><br />"
-            result += "<pre class=\"c-code c-code--multiline\">" + section[1] + "</pre>"
+            result += "<pre class=\"c-code c-code--multiline\">" + section[1].strip() + "</pre>"
             result += "</p>"
 
     return result
