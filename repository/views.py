569b7a2ae1251d3387e28e68633592ad2ae58124
cty123 <chen2216@purdue.edu>
2018-04-09 21:52:08 -0400

    Implement file page that can create file repositories.

---
+++ repository/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from repository.models import Repository
+# Create your views here.
+
+
+class RepositoryView(View):
+    @method_decorator(login_required(login_url='login'))
+    def get(self, request):
+        user = request.user
+        repos = Repository.objects.filter(user=user)
+        return render(request, 'files.html', {'repos': repos})
+
+    @method_decorator(login_required(login_url='login'))
+    def post(self, request):
+        repo_owner = request.user
+        repo_name = request.POST.get("repo_name", "")
+        repo = Repository(name=repo_name, user=repo_owner)
+        repo.set_path()
+        repo.save()
+        print('repo path is' + repo.repo_path)
+        return self.get(request=request)
