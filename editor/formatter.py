e1657db8b996b58e972501f9f080e8e0fb4ef715
achesak <achesak@yahoo.com>
2018-04-11 12:53:10 -0400

    Add counter to output

--- editor/formatter.py
+++ editor/formatter.py
@@ -4,9 +4,13 @@ def format(output):
     result = ""
     for section in output:
         if section[1] and section[1].strip() != "":
-            result += "<p><h1 class=\"c-heading u-small\">" + section[0] + "</h1><br />"
+            result += "<p><h1 class=\"c-heading u-small\">" + str(format.counter) + ") " + section[0] + "</h1><br />"
             result += "<pre class=\"c-code c-code--multiline\">" + section[1].strip() + "</pre>"
             result += "</p>"
 
+    format.counter += 1
+
     return result
 
+
+format.counter = 1
