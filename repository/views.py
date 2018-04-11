2e177e2d0f957e8df071c0f7165f43c20f5ea456
achesak <achesak@yahoo.com>
2018-04-11 15:42:04 -0400

    Add repository routes

--- repository/views.py
+++ repository/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
+from django.http import JsonResponse, HttpResponse
 from repository.models import Repository
 
 import re
+import os
 
 
 class RepositoryView(View):
@@ -29,3 +31,59 @@ class RepositoryView(View):
         repo.save()
         print('repo path is' + repo.repo_path)
         return self.get(request=request)
+
+
+class RepositoryFileView(View):
+    @method_decorator(login_required(login_url='login'))
+    def get(self, request):
+        """Gets the files in a repository."""
+
+        path = request.GET.get("path", "")
+        mode = request.GET.get("mode", "")
+
+        if mode == "list":
+            files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and os.access(os.path.join(path, f), os.X_OK)]
+            files.sort()
+
+            return JsonResponse(files, safe=False)
+
+        else:
+            filename = request.GET.get("filename", "")
+
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+
+            if not os.path.isdir(path):
+                os.makedirs(path)
+
+            os.chdir(path)
+
+            f = open(filename, "r")
+            text = f.read()
+            f.close()
+
+            os.chdir(current_dir)
+
+            return HttpResponse(text)
+
+    @method_decorator(login_required(login_url='login'))
+    def post(self, request):
+        """Saves a file to the directory."""
+
+        path = request.POST.get("path", "")
+        filename = request.POST.get("filename", "")
+        text = request.POST.get("text")
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+        os.chdir(path)
+
+        f = open(filename, "w")
+        f.write(text)
+        f.close()
+
+        os.chdir(current_dir)
+
+        return HttpResponse("File saved")
