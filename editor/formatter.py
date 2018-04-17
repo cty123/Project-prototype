ef7a1fc6e19ffa58c1265c02c95f837d83eb449b
achesak <achesak@yahoo.com>
2018-04-17 14:38:08 -0400

    Minor formatting fix

--- editor/formatter.py
+++ editor/formatter.py
@@ -2,11 +2,11 @@ def format(output):
     """Formats the code output to look nicer."""
 
     result = "<div style=\"margin-bottom: 20px\" class=\"c-card\">"
-    result += "<header class=\"c-card__header\"><h2 class=\"c-heading\">Output"
+    result += "<header class=\"c-card__header\" style=\"margin-bottom: 10px\"><h2 class=\"c-heading\">Output"
     result += " <a class=\"remove_output\" style=\"cursor: pointer; color: red\">&times;</a></h2></header>"
     for section in output:
         if section[1] and section[1].strip() != "":
-            result += "<div style=\"margin-top: 10px; padding-left: 20px\" class=\"c-card__item c-card__item--divider "
+            result += "<div style=\"padding-left: 20px\" class=\"c-card__item c-card__item--divider "
             if section[0] == "Execution Output":
                 result += "c-card__item--success"
             elif section[0] == "Compiler Output":
